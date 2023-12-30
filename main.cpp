faf6ea5259ca643d68d804d77b799d588cffa168
vkobinski <victorkobinski@outlook.com>
2023-12-30 13:02:32 -0300

    Red Triangle

--- main.cpp
+++ main.cpp
@@ -1,100 +1,203 @@
+#include <vector>
+#include <cmath>
 #include <SDL.h>
-#include <SDL_opengl.h>
 #include <iostream>
 
-SDL_Window* window = nullptr;
-SDL_GLContext context;
+class Point {
+public:
+    double x, y, z;
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-
-bool init_sdl() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "SDL could not initialize! SDL_Error: %s\n" << SDL_GetError();
-        return false;
+    Point() {
+        this->x = 0;
+        this->y = 0;
+        this->z = 0;
     }
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-
-    window = SDL_CreateWindow("3D Cube Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-    if (window == nullptr)
-    {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        return false;
+    Point(double x, double y, double z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
     }
 
-    context = SDL_GL_CreateContext(window);
-    if (context == nullptr)
-    {
-        printf("OpenGL context could not be created! SDL_Error: %s\n", SDL_GetError());
-        return false;
+    Point(double x, double y) {
+        this->x = x;
+        this->y = y;
+        this->z = 0;
     }
 
-    return true;
+    double operator[](int i) const {
+        if (i == 0) return this->x;
+        if (i == 1) return this->y;
+        return this->z;
+    }
+
+    double& operator[](int i) {
+        if (i == 0) return this->x;
+        if (i == 1) return this->y;
+        return this->z;
+    }
+};
+
+typedef std::vector<double> Vector;
+typedef std::vector<Vector> Matrix;
+
+Matrix dot(const Matrix& a, const Matrix& b) {
+    Matrix result = Matrix(a.size(), Vector(b[0].size(), 0));
+    for (int i=0; i<a.size(); i++) {
+        for (int j=0; j<b[0].size(); j++) {
+            for (int k=0; k<b.size(); k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    return result;
 }
 
-void render_cube() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBegin(GL_QUADS);
-
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, 1.0f, 1.0f);
-    glVertex3f(-1.0f, 1.0f, 1.0f);
-
-    // Back face
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glVertex3f(1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, -1.0f);
-
-    // ... repeat for other faces
-
-    glEnd();
-
-    SDL_GL_SwapWindow(window);
-
+Point transform(const Matrix& matrix, const Point& point) {
+    Matrix p = {{point.x}, {point.y}, {point.z}};
+    Matrix r = dot(matrix, p);
+    return Point(r[0][0], r[1][0], r[2][0]);
 }
 
-void clean_up() {
-    SDL_GL_DeleteContext(context);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+Point translate(const Point& shift, const Point& point) {
+    return Point(
+            point.x + shift.x,
+            point.y + shift.y,
+            point.z + shift.z
+    );
+}
+
+void connect(SDL_Renderer* const renderer, const std::vector<Point> &points, int i, int j) {
+    SDL_RenderDrawLine(
+            renderer,
+            points[i].x,
+            points[i].y,
+            points[j].x,
+            points[j].y
+    );
+}
+
+void connect_triangles(SDL_Renderer* const renderer, const std::vector<Point> &points, int i, int j, int k) {
+
+    SDL_Vertex vertices[3];
+    int indexes[3] = {i,j,k};
+
+    for(int cur = 0; cur < 3; cur++ ) {
+        SDL_FPoint point = {(float) points[indexes[cur]].x, (float) points[indexes[cur]].y};
+        SDL_Vertex vertex = {{point}, {255, 0, 0, 255}, {1, 1}};
+
+        vertices[cur] = vertex;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_RenderGeometry(renderer, nullptr, vertices, 3, nullptr, 0);
+}
+
+Matrix getRotationMatrix() {
+    double alpha = 0.001;
+    Matrix rotationX = {
+            {1, 0, 0},
+            {0, cos(alpha), -sin(alpha)},
+            {0, sin(alpha), cos(alpha)}
+    };
+
+    double beta = 0.002;
+    Matrix rotationY = {
+            {cos(beta), 0, sin(beta)},
+            {0, 1, 0},
+            {-sin(beta), 0, cos(beta)}
+    };
+
+    double gamma = 0.003;
+    Matrix rotationZ = {
+            {cos(gamma), -sin(gamma), 0},
+            {sin(gamma), cos(gamma), 0},
+            {0, 0, 1}
+    };
+
+    return dot(rotationZ, dot(rotationY, rotationX));
 }
 
 int main(int argc, char** argv) {
-
-    if(!init_sdl()) {
-        std::cout << "Failed to initialize SDL!\n";
-        return -1;
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
     }
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+    int WIDTH = 1000;
+    int HEIGHT = 1000;
 
-    bool quit = false;
-    SDL_Event e;
+    SDL_Window* window = SDL_CreateWindow(
+            "GAME",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            WIDTH,
+            HEIGHT,
+            0
+    );
 
-    glEnable(GL_DEPTH_TEST);
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+            window,
+            -1,
+            SDL_RENDERER_ACCELERATED
+    );
 
-    while (!quit)
-    {
-        while (SDL_PollEvent(&e) != 0)
-        {
-            if (e.type == SDL_QUIT)
-            {
-                quit = true;
+    std::vector<Point> points = {
+            Point(-1, 1, 1),
+            Point(1, 1, 1),
+            Point(1, -1, 1),
+            Point(-1, -1, 1),
+            Point(-1, 1, -1),
+            Point(1, 1, -1),
+            Point(1, -1, -1),
+            Point(-1, -1, -1)
+    };
+
+    Point screenShift(WIDTH / 2, HEIGHT / 2);
+    Point screenShiftOpposite(-WIDTH / 2, -HEIGHT / 2);
+    int scale = 200;
+
+    for (Point& p : points) {
+        p.x = (scale * p.x + screenShift.x);
+        p.y = (scale * p.y + screenShift.y);
+        p.z = (scale * p.z + screenShift.z);
+    }
+
+    Matrix rotationXYZ = getRotationMatrix();
+
+    bool close = false;
+    while (!close) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                close = true;
             }
         }
 
-        render_cube();
+        for (Point &p : points) {
+            p = translate(screenShiftOpposite, p);
+            p = transform(rotationXYZ, p);
+            p = translate(screenShift, p);
+        }
+
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        for (int i=0; i<4; i++) {
+
+            connect(renderer, points, i, (i + 1) % 4);
+            connect(renderer, points, i + 4, ((i + 1) % 4) + 4);
+            connect(renderer, points, i, i + 4);
+        }
+
+        connect_triangles(renderer, points, 0,1,2);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(3);
     }
 
-
+    SDL_DestroyWindow(window);
     SDL_Quit();
-
-    return 0;
 }
